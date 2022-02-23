aa58c0d7100d6fff8df2f03d1f0640e98781d753
Aditya Narayan Mishra <mishraaditya384@gmail.com>
2022-02-23 16:11:58 +0530

    Adding Dynamic Views

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
+const { getProducts } = require('../workers');
 var router = express.Router();
 /* GET home page. */
 router.get('/', async function (req, res, next) {
 
-
-  res.render('index', { title: 'Express' });
+  const prods = await getProducts();
+  res.render('index', { title: 'OpsWorks', prods });
 });
 
 module.exports = router;
